182c96e36d72993a8c173ff965325026d2d805e2
Lukas <21Nuga@pm.me>
2025-03-21 18:08:21 +0100

    created the main.py code

---
+++ main.py
@@ -0,0 +1,34 @@
+import products
+import store
+from console import enter_to_continue, get_valid_number_from_user, display_products, display_menu, \
+    display_total_quantity, order_process
+
+# setup initial stock of inventory
+product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                products.Product("Google Pixel 7", price=500, quantity=250)
+                ]
+
+
+def main():
+    best_buy = store.Store(product_list)
+    while True:
+        display_menu()
+        user_input = get_valid_number_from_user(1, 4)
+
+        match user_input:
+            case 1:
+                display_products(best_buy)
+                enter_to_continue()
+            case 2:
+                display_total_quantity(best_buy)
+                enter_to_continue()
+            case 3:
+                order_process(best_buy)
+                enter_to_continue()
+            case 4:
+                break
+
+
+if __name__ == "__main__":
+    main()
